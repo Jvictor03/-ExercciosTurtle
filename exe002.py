8979a70061bc33c22566852951f1840a1b80d64c
Jvictor03 <133661555+Jvictor03@users.noreply.github.com>
2023-10-09 12:32:42 -0300

    Add files via upload

---
+++ exe002.py
@@ -0,0 +1,19 @@
+"""
+Exercícios
+
+1) Mude a cor da linha
+2) Mude o formato da tartaruga
+3) Mude o tamanho do quadrado
+"""
+
+import turtle
+
+turtle = turtle.Turtle()
+turtle.color('yellow')
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
